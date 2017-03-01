654b20d5943c52b42e6efd68bb62831a0b694fd2
Kavya Vemula <kavya.vemula1@marist.edu>
2017-03-01 04:24:47 -0500

    Formatted according to style guidelines.

--- labs/Lab 02/Euclidean.java
+++ labs/Lab 02/Euclidean.java
@@ -15,8 +15,6 @@ class Euclidean
 	static int[] input= new int[50];
 	public static void main(String[] args){
     Scanner sc= new Scanner(System.in);
-		//System.out.println("Enter the number of lines");
-		//int lines= sc.nextInt();
 		System.out.println("Enter the pair of digits for which you want to apply Euclidean Algorithm.");
 		System.out.println("Press ctrl+c (for windows) or command+c (for mac) once you are done entering");
 		while(sc.hasNext()){
