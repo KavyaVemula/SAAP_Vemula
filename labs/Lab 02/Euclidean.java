c20ff3f59ba559552b0161ee8d2692ea249217e0
Kavya Vemula <kavya.vemula1@marist.edu>
2017-03-04 13:15:16 -0500

    Update Euclidean.java

--- labs/Lab 02/Euclidean.java
+++ labs/Lab 02/Euclidean.java
@@ -10,41 +10,41 @@
 import java.util.Scanner;
 class Euclidean
 {
-	static int q1, r1;
-	static int count=0;
-	static int[] input= new int[50];
-	public static void main(String[] args){
+  static int q1, r1;
+  static int count=0;
+  static int[] input= new int[50];
+  public static void main(String[] args){
     Scanner sc= new Scanner(System.in);
-		System.out.println("Enter the pair of digits for which you want to apply Euclidean Algorithm.");
-		System.out.println("Press ctrl+c (for windows) or command+c (for mac) once you are done entering");
-		while(sc.hasNext()){
-		  input[count]= sc.nextInt();
+    System.out.println("Enter the pair of digits for which you want to apply Euclidean Algorithm.");
+    System.out.println("Press ctrl+c (for windows) or command+c (for mac) once you are done entering");
+    while(sc.hasNext()){
+      input[count]= sc.nextInt();
       count++;
-		}
-		for(int i=0;i<count-1;i+=2){
-		  euclidAlg(input[i],input[i+1]);
-		}
+    }
+    for(int i=0;i<count-1;i+=2){
+      euclidAlg(input[i],input[i+1]);
+    }
   }
-	/**
-   * euclidAlg
-   *
-   * This is a recursive function that calculates gcd
-   * The base condition here is until remainder becomes 0
-   * Parameters:
-   *   input[]: Digits entered in each line
-   *
-   * Return value:
-   *   GCD of the entered digits
-   */
-	public static void euclidAlg(int a, int b){
-	  q1= a/b;
-		r1= a%b;
-		if(r1==0)
-		  System.out.println(b);
-		else{
-		  a=b;
-			b=r1;
-			euclidAlg(a,b);
-		}
-	}
+  /**
+  * euclidAlg
+  *
+  * This is a recursive function that calculates gcd
+  * The base condition here is until remainder becomes 0
+  * Parameters:
+  *   input[]: Digits entered in each line
+  *
+  * Return value:
+  *   GCD of the entered digits
+  */
+  public static void euclidAlg(int a, int b){
+    q1= a/b;
+    r1= a%b;
+    if(r1==0)
+      System.out.println(b);
+    else{
+      a=b;
+      b=r1;
+      euclidAlg(a,b);
+    }
+  }
 }
